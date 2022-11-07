0a125f1f87e70f7970c17168c66de618e6c9ff21
Sabin Subedi <sabinsubedi125@gmail.com>
2022-11-07 12:31:49 +0545

    remix initial setup done

---
+++ app/root.tsx
@@ -0,0 +1,17 @@
+import { Links, LiveReload, Outlet } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export default function IndexRoute() {
+  return <div>Hello Index Route</div>;
+}
